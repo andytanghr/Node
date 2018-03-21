a2d97ac20e32bd4139124efb0f7ac460ae694090
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-03-21 11:34:49 -0500

    finished expressExercises.js

---
+++ expressExercises.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res) {
+  res.send('Hello, world!');
+});
+
+app.get('/cats', function(req, res) {
+  res.send('Meow');
+
+});
+
+app.get('/dogs', function(req, res) {
+  res.send('Woof');
+});
+
+app.get('/cats_and_dogs', function(req, res) {
+  res.send('Living together');
+});
+
+app.get('/greet/:name', function(req, res) { // routing parameter method
+  var name = req.params.name;
+  res.send('Hello, ' + name + '!');
+});
+
+app.get('/year', function(req, res) { // query string parameter method
+  var age = req.query.age;
+  var year = new Date().getFullYear();
+  age = year - age;
+  res.send('You were born in ' + age + '.');
+});
+
+// examples of routing and query parameter methods: https://stackoverflow.com/questions/17007997/how-to-access-the-get-parameters-after-in-express/38201623#38201623
+
+
+app.listen(3000, function() {
+  console.log('Express exercises started on port 3000!');
+});
--- learnyounode/filteredLS.js
+++ learnyounode/filteredLS.js
@@ -1,18 +1,15 @@
 var fs = require('fs');
-var path = require('path');
-var directory = process.argv[2];
-var extension = '.' + process.argv[3];
+var path = require('path'); // module to grab the file extension, .extname
+var directory = process.argv[2]; // directory path grabbed from second cmd line argument
+var extension = '.' + process.argv[3]; // prefixed '.' and the extname is grabbed from the third cmd line argument
 
 fs.readdir(directory, function(err, files) {
   if (err) {
     console.error(error.message);
   }
-
   files.forEach(function(file) {
     if (path.extname(file) === extension) {
       console.log(file);
     }
-
   });
-
 });
